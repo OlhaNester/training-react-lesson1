1ec8e48c3507430a29fb67344a2a6d017fe9a488
OlhaNester <olga_nv@ukr.net>
2021-09-23 21:49:41 +0300

    222

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 // import ColorPicker from './ColorPicker';
-import TodoList from './TodoList';
+import TodoList from "./TodoList";
 
 // const colorPickerOpt = [
 //   { label: 'red', color: '#F44336' },
@@ -14,36 +14,48 @@ import TodoList from './TodoList';
 class App extends Component {
   state = {
     todos: [
-      { id: 'id-1', text: 'text-1', completed: false },
-      { id: 'id-2', text: 'text-2', completed: true },
-      { id: 'id-3', text: 'text-3', completed: true },
-      { id: 'id-4', text: 'text-4', completed: false },
+      { id: "id-1", text: "text-1", completed: false },
+      { id: "id-2", text: "text-2", completed: true },
+      { id: "id-3", text: "text-3", completed: true },
+      { id: "id-4", text: "text-4", completed: false },
     ],
   };
-  deleteTodo = ( todoId ) => {
-     
-        this.setState(prevState => ({ todos: prevState.todos.filter(todo => todo.id !== todoId), }));
-    
+  deleteTodo = (todoId) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.filter((todo) => todo.id !== todoId),
+    }));
   };
 
-  
-  
   render() {
     const { todos } = this.state;
     const totalTodo = todos.length;
-    const complitedTodo = todos.reduce((acc, todo) => ( todo.completed ? acc + 1 : acc ), 0);
-    return (<div className="App">
-      <div>
-        <p>Общее кол-во { totalTodo}</p>
-        <p> Кол-во выполненных { complitedTodo}</p>
+    const complitedTodo = todos.reduce(
+      (acc, todo) => (todo.completed ? acc + 1 : acc),
+      0
+    );
+    return (
+      <div className="App">
+        <form>
+          <p>Ваш уровень:</p>
+          <label>
+            <input type="radio" name="" value="junior"></input>junior
+          </label>
+          <label>
+            <input type="radio" name="" value="middle"></input>middle
+          </label>
+          <label>
+            <input type="radio" name="" value="senior"></input>senior
+          </label>
+        </form>
+        <div>
+          <p>Общее кол-во {totalTodo}</p>
+          <p> Кол-во выполненных {complitedTodo}</p>
+        </div>
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+        {/* <ColorPicker options={colorPickerOpt}/> */}
       </div>
-      <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
-      {/* <ColorPicker options={colorPickerOpt}/> */}
-    </div>);
-  
+    );
+  }
 }
-};
-
-
 
 export default App;
