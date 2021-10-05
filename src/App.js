b343998678e1cb7a6f0fb23ca82bff333d1a435c
OlhaNester <olga_nv@ukr.net>
2021-10-05 22:28:06 +0300

    модальное окно

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
-import ColorPicker from './ColorPicker';
+import ColorPicker from "./ColorPicker";
 import TodoList from "./TodoList";
 import Form from "./Form";
 import TodoEditor from "./TodoEditor";
 import shortid from "shortid";
-import Modal from './Modal';
-import Clock from './Clock';
+import Modal from "./Modal";
+import Clock from "./Clock";
 //import { findRenderedDOMComponentWithClass } from "react-dom/test-utils";
 
-
 const colorPickerOpt = [
-  { label: 'red', color: '#F44336' },
-  { label: 'green', color: '#4CAF50' },
-  { label: 'blue', color: '#2196F3' },
-  { label: 'grey', color: '#607D8B' },
-  { label: 'pink', color: '#E91E63' },
-  { label: 'indigo', color: '#3F51B5' },
+  { label: "red", color: "#F44336" },
+  { label: "green", color: "#4CAF50" },
+  { label: "blue", color: "#2196F3" },
+  { label: "grey", color: "#607D8B" },
+  { label: "pink", color: "#E91E63" },
+  { label: "indigo", color: "#3F51B5" },
 ];
 
 class App extends Component {
@@ -30,57 +29,57 @@ class App extends Component {
       // { id: "id-3", text: "text-3", completed: true },
       // { id: "id-4", text: "text-4", completed: false },
     ],
-
   };
   componentDidMount() {
-       const todos = localStorage.getItem('todos');
+    const todos = localStorage.getItem("todos");
     const parseTodos = JSON.parse(todos);
-    if (parseTodos) { this.setState({ todos: parseTodos });}
-    
-  };
+    if (parseTodos) {
+      this.setState({ todos: parseTodos });
+    }
+  }
 
   componentDidApdate(prevProps, prevState) {
     if (this.state.todos !== prevState.todos) {
-      console.log("Обновилось поле")
-      localStorage.setItem('todos', JSON.stringify(this.state.todos));
-
+      console.log("Обновилось поле");
+      localStorage.setItem("todos", JSON.stringify(this.state.todos));
     }
-    
-  };
+  }
 
   addTodo = (text) => {
-    
     const todo = {
       id: shortid.generate(),
       text: text,
       completed: false,
-      
-    }
-    this.setState(prevState => ({
-  todos: [todo, ...prevState.todos]
-}))
-
+    };
+    this.setState((prevState) => ({
+      todos: [todo, ...prevState.todos],
+    }));
   };
 
   deleteTodo = (todoId) => {
-     
-    this.setState(prevState =>
-      ({ todos: prevState.todos.filter(todo => todo.id !== todoId), }));
+    this.setState((prevState) => ({
+      todos: prevState.todos.filter((todo) => todo.id !== todoId),
+    }));
   };
 
-  toggleCompleted = todoId => {
+  toggleCompleted = (todoId) => {
     console.log(todoId);
-  
-  this.setState(prevState => ({                                     // можно переписать в виде тернарника:
-    todos: prevState.todos.map(todo => {                            // this.setState(prevState =>
-      if (todo.id === todoId) {                                     // todos: prevState.todos.map(todo => {
-        return {                                                    // todo.id === todoId ?
-          ...todo, completed: !todo.completed,                      // {  ...todo, completed: !todo.completed,} : todo
-        };
-      }
-      return todo;  //если id не равен, мы возвращаем старый todo
-    }),
-  }));
+
+    this.setState((prevState) => ({
+      // можно переписать в виде тернарника:
+      todos: prevState.todos.map((todo) => {
+        // this.setState(prevState =>
+        if (todo.id === todoId) {
+          // todos: prevState.todos.map(todo => {
+          return {
+            // todo.id === todoId ?
+            ...todo,
+            completed: !todo.completed, // {  ...todo, completed: !todo.completed,} : todo
+          };
+        }
+        return todo; //если id не равен, мы возвращаем старый todo
+      }),
+    }));
   };
   toggleModal = () => {
     this.setState(({ showModal }) => ({
@@ -90,11 +89,9 @@ class App extends Component {
 
   handleChange = (event) => {
     const { name, value } = event.currentTarget;
-        this.setState({ [name]: value });
+    this.setState({ [name]: value });
   };
 
-  
-  
   // handleNameChange = (event) => {
   //   this.setState({ name: event.currentTarget.value });
   // }
@@ -102,45 +99,71 @@ class App extends Component {
   // handleTagChange = (event) => {
   //   this.setState({ tag: event.currentTarget.value });
   // }
-  
+
   render() {
     const { todos, showModal } = this.state;
     const totalTodo = todos.length;
-    const complitedTodo = todos.reduce((acc, todo) => (todo.completed ? acc + 1 : acc), 0);
+    const complitedTodo = todos.reduce(
+      (acc, todo) => (todo.completed ? acc + 1 : acc),
+      0
+    );
     return (
-      
       <div className="App">
-        <button type="button" onClick={this.toggleModal}>Открыть модалку</button>
-        <Clock/>
-        {showModal && <Modal onClose = {this.toggleModal}>
-          <h1>Это модальное окно</h1>
-          <p>Lorem Ipsum";
-          </p>
-          <button type="button" onClick={this.toggleModal}>Закрыть модалку</button>
-        </Modal>}
-
+        <button type="button" onClick={this.toggleModal}>
+          Открыть модалку
+        </button>
+        <Clock />
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            {/* <h1>Это модальное окно</h1>
+            <p>Lorem Ipsum";</p> */}
+            <TodoEditor onSubmit={this.addTodo} />
+            <button type="button" onClick={this.toggleModal}>
+              Закрыть модалку
+            </button>
+          </Modal>
+        )}
         <Form />
+
         <form>
-           <label> Имя
-      <input type="text" name="name" value={this.state.name} onChange={this.handleChange} id={this.nameImputId} />
-    </label>
-    <label> Ник   <input type="text" name="tag" value={this.state.tag} onChange={this.handleChange} id={this.nameImputId} />
-    </label>
-  </form>
-      
-        
+          <label>
+            {" "}
+            Имя
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              id={this.nameImputId}
+            />
+          </label>
+          <label>
+            {" "}
+            Ник{" "}
+            <input
+              type="text"
+              name="tag"
+              value={this.state.tag}
+              onChange={this.handleChange}
+              id={this.nameImputId}
+            />
+          </label>
+        </form>
 
         <div>
           <p>Общее кол-во {totalTodo}</p>
           <p> Кол-во выполненных {complitedTodo}</p>
         </div>
-        <TodoEditor onSubmit={ this.addTodo}/>
-        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} onToggleCompleted={this.toggleCompleted} />
+
+        <TodoList
+          todos={todos}
+          onDeleteTodo={this.deleteTodo}
+          onToggleCompleted={this.toggleCompleted}
+        />
         <ColorPicker options={colorPickerOpt} />
       </div>
-    )
-  
-};
+    );
+  }
 }
 
 export default App;
--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,38 +1,36 @@
-import { render } from '@testing-library/react';
-import React, { Component } from 'react';
-import { createPortal } from 'react-dom';
-import './Modal.css';
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+import { createPortal } from "react-dom";
+import "./Modal.css";
 
 const modalRoot = document.querySelector("#modalRoot");
 
 export default class Modal extends Component {
-    componentDidMount() {
-        console.log("Modal componentDidMount");
-        window.addEventListener("keydown", this.handleKeyDown);
+  componentDidMount() {
+    console.log("Modal componentDidMount");
+    window.addEventListener("keydown", this.handleKeyDown);
+  }
+  componentWillUnmount() {
+    console.log("Modal componentWillUnmount");
+    window.removeEventListener("keydown", this.handleKeyDown);
+  }
+  handleKeyDown = (event) => {
+    if (event.code === "Escape") {
+      this.props.onClose();
     }
-    componentWillUnmount() {
-        console.log('Modal componentWillUnmount');
-        window.removeEventListener("keydown", this.handleKeyDown)
+  };
+  handleBackDrop = (event) => {
+    if (event.currentTarget === event.target) {
+      this.props.onClose();
     }
-    handleKeyDown = (event) =>  {
-        if (event.code === "Escape") {
-            this.props.onClose()
-        }
-    };
-    handleBackDrop = (event) => {
-        if (event.currentTarget === event.target) {
-            this.props.onClose();
-        }
-    }
-   
+  };
 
-render() {
-    return createPortal(<div className="Modal_backdrop" onClick={this.handleBackDrop}>
-            <div className="Modal_content">{this.props.children}</div>
-        </div>, modalRoot)
-        
-
-    
+  render() {
+    return createPortal(
+      <div className="Modal_backdrop" onClick={this.handleBackDrop}>
+        <div className="Modal_content">{this.props.children}</div>
+      </div>,
+      modalRoot
+    );
+  }
 }
-}
-
